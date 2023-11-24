e8fcb150153c8652a42179f9d7d38b4d0d4e1c46
Dev. Alex_dgreat <105602085+devalexdgreat@users.noreply.github.com>
2023-11-23 19:14:28 -0800

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Navbar from '@/components/Navbar'
+import TrackCard from '@/components/TrackCard'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="">
+      <Navbar />
+      <TrackCard />
+    </main>
+  )
+}
